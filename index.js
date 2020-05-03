0f717df2be0b39b05034996537c9b5d7e15b283b
Thomas Andre <thomasandre84@gmail.com>
2020-05-03 11:44:18 +0200

    add for fileUpload

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const app = express();
 const port = 3000;
 
-const db = require("./queries");
+const db = require("./database");
 
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
+  })
+);
+app.use(cors);
+app.use(
+  fileUpload({
+    debug: true,
   })
 );
 
@@ -22,3 +30,24 @@ app.get("/", (request, response) => {
 
 app.get("/quotes", db.getQuotes);
 app.get("/quotes/random", db.getRandomQuote);
+
+app.post("/quotes/upload", (req, res) => {
+  const image = req.files.myFile;
+  const path = __dirname + "/tmp" + image.name;
+
+  image.mv(path, (error) => {
+    if (error) {
+      console.error(error);
+      res.writeHead(500, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ status: "error", message: error }));
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(JSON.stringify({ status: "success", path: "/tmp" + image.name }));
+  });
+});
